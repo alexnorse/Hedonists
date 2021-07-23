11ffb5fde5c4a680aa08bf2ab22da2fadd1a0b88
Alexandr <alexandrlobanov@outlook.com>
2021-07-23 10:20:18 +0300

    MapView and user location setup

--- PresnyaHedonist/Controllers/FullmapVC.swift
+++ PresnyaHedonist/Controllers/FullmapVC.swift
@@ -6,15 +6,30 @@
 //
 
 import UIKit
+import CoreLocation
 import MapKit
+import CoreData
 
-class FullmapVC: UIViewController {
+class FullmapVC: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        locationManager.desiredAccuracy     = kCLLocationAccuracyBest
+        locationManager.delegate            = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         configureController()
@@ -22,8 +37,32 @@ class FullmapVC: UIViewController {
     
     
     func configureController() {
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.navigationBar.titleTextAttributes = ([NSAttributedString.Key.font: Fonts.vcHeads!])
-        title = VCTitles.mapTitle
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            render(location)
+        }
+    }
+    
+    
+    func render(_ location: CLLocation) {
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                                longitude: location.coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.1,
+                                    longitudeDelta: 0.1)
+        
+        let region = MKCoordinateRegion(center: coordinate,
+                                        span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        let myPin = MKPointAnnotation()
+        myPin.coordinate = coordinate
+        mapView.addAnnotation(myPin)
     }
 }
