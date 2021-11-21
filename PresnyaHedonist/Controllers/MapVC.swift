374488ce6f66c92b989728dc299d8cb3281afec8
Alexandr <alexandrlobanov@outlook.com>
2021-11-21 15:13:25 +0300

    MapZoom setup

--- PresnyaHedonist/Controllers/MapVC.swift
+++ PresnyaHedonist/Controllers/MapVC.swift
@@ -143,11 +143,11 @@ extension MapVC: CLLocationManagerDelegate {
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         guard let annotiation = view.annotation else { return }
         
-        let directionRequest = MKDirections.Request()
-        directionRequest.source = MKMapItem.forCurrentLocation()
-        directionRequest.destination = MKMapItem(placemark: MKPlacemark(coordinate: annotiation.coordinate))
-        directionRequest.transportType = .walking
-        let directions = MKDirections(request: directionRequest)
+        let request = MKDirections.Request()
+        request.source = MKMapItem.forCurrentLocation()
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: annotiation.coordinate))
+        request.transportType = .walking
+        let directions = MKDirections(request: request)
         
         directions.calculate { (response, error) -> Void in
             guard let response = response else {
@@ -157,7 +157,11 @@ extension MapVC: CLLocationManagerDelegate {
             
             if !response.routes.isEmpty {
                 let route = response.routes[0]
-                DispatchQueue.main.async { [weak self] in self?.mapView.addOverlay(route.polyline) }
+                
+                DispatchQueue.main.async {
+                    self.mapView.addOverlay(route.polyline)
+                    self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+                }
             }
         }
     }
